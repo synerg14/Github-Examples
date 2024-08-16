f1de4a2afb178f0ab6f94815a3dceb756c15c8bf
synerg14 <synergia.soporte@gmail.com>
2024-08-16 00:52:48 -0300

    use SDK with ruby and JS

--- github-sdks/js/main.js
+++ github-sdks/js/main.js
@@ -1,17 +1,17 @@
-const { Octokit, App } = require("octokit");
+import("octokit").then(({ Octokit, App }) => {
+  console.log(`TOKEN: ${process.env.GH_TOKEN}`);
 
-console.log(`TOKEN: ${process.env.GH_TOKEN}`)
+  const octokit = new Octokit({
+    auth: process.env.GH_TOKEN,
+  });
 
-const octokit = new Octokit({
-    auth: process.env.GH_TOKEN
-  })
-  
-octokit.request('POST /repos/{owner}/{repo}/git/refs', {
-    owner: 'andrew-wc-brown',
-    repo: 'Github-Examples',
-    ref: 'refs/heads/sdksjs',
-    sha: '3a652b86905ca4e7888f4898e1b6f5d957b2c926',
+  octokit.request("POST /repos/{owner}/{repo}/git/refs", {
+    owner: "synerg14",
+    repo: "Github-Examples",
+    ref: "refs/heads/sdksjs",
+    sha: "3a652b86905ca4e7888f4898e1b6f5d957b2c926",
     headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+});
--- github-sdks/ruby/main.rb
+++ github-sdks/ruby/main.rb
@@ -4,7 +4,7 @@ client = Octokit::Client.new(access_token: ENV['GH_TOKEN'])
 
 # Create the new branch
 client.create_ref(
-"andrew-wc-brown/Github-Examples", 
+"synerg14/Github-Examples", 
 "heads/sdks", 
 "3a652b86905ca4e7888f4898e1b6f5d957b2c926"
 )
